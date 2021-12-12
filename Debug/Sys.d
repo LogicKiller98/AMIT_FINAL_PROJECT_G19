95ca123dd16486a286a8923726c0bed1523ca6e4
LogicKiller98 <ahmedmostafa823@hotmail.com>
2021-12-12 23:23:10 +0200

    v1.2

---
+++ Debug/Sys.d
@@ -0,0 +1,29 @@
+Sys.o Sys.o: ../Sys.c ../Sys.h ../BIT_MATH.h ../ADC.h ../ADC_Private.h \
+  ../STD_TYPES.h ../Buttons.h ../GIE_Interface.h ../Cool_Heat_Elements.h \
+  ../LED.h ../SSD_disp.h ../Timer.h ../GIE_Interface.H ../EEPROM.h
+
+../Sys.h:
+
+../BIT_MATH.h:
+
+../ADC.h:
+
+../ADC_Private.h:
+
+../STD_TYPES.h:
+
+../Buttons.h:
+
+../GIE_Interface.h:
+
+../Cool_Heat_Elements.h:
+
+../LED.h:
+
+../SSD_disp.h:
+
+../Timer.h:
+
+../GIE_Interface.H:
+
+../EEPROM.h:
---
+++ Release/System_control.d
@@ -0,0 +1,30 @@
+System_control.o System_control.o: ../System_control.c \
+  ../System_control.h ../BIT_MATH.h ../ADC.h ../ADC_Private.h \
+  ../STD_TYPES.h ../Buttons.h ../GIE_Interface.h ../Cool_Heat_Elements.h \
+  ../LED.h ../SSD_disp.h ../Timer.h ../GIE_Interface.H ../EEPROM.h
+
+../System_control.h:
+
+../BIT_MATH.h:
+
+../ADC.h:
+
+../ADC_Private.h:
+
+../STD_TYPES.h:
+
+../Buttons.h:
+
+../GIE_Interface.h:
+
+../Cool_Heat_Elements.h:
+
+../LED.h:
+
+../SSD_disp.h:
+
+../Timer.h:
+
+../GIE_Interface.H:
+
+../EEPROM.h:
